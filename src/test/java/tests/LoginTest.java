9a69b2f73a2188458a0c135090327f4187ed92d4
Borodich Timofei <borodich.timofei@gmail.com>
2025-07-28 17:08:49 +0300

    Initial commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void checkLoginWithoutPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Password is required",
+                "Сообщение не соотвествует");
+    }
+
+    @Test
+    public void checkLoginWithoutUsername() {
+        loginPage.open();
+        loginPage.login("", "secret_sauce");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username is required",
+                "Сообщение не соотвествует");
+    }
+
+    @Test
+    public void checkLoginWithNegativeValue() {
+        loginPage.open();
+        loginPage.login("test", "test");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username and password do not match any user in this service",
+                "Сообщение не соотвествует");
+    }
+
+    @Test
+    public void checkLogin() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        productsPage.addToCart("");
+        productsPage.addToCart("");
+        productsPage.addToCart("");
+    }
+}
